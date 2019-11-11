e37a43f80a6c67417e1604abe10ed940782a300b
MajemiteJames <majemiteokoro@gmail.com>
2019-11-11 16:15:09 +0100

    Updates

---
+++ precache-manifest.3bb3932ce8715fcc5905701fac11fba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65652e00d4963852ce7cd4d9b9144c52",
+    "url": "/monster-rolodex-course/index.html"
+  },
+  {
+    "revision": "5eca1fdddde18c7cfe2a",
+    "url": "/monster-rolodex-course/static/css/main.d15dc59a.chunk.css"
+  },
+  {
+    "revision": "391cb0e4e182aa14b4b0",
+    "url": "/monster-rolodex-course/static/js/2.5430253d.chunk.js"
+  },
+  {
+    "revision": "5eca1fdddde18c7cfe2a",
+    "url": "/monster-rolodex-course/static/js/main.35d89cf0.chunk.js"
+  },
+  {
+    "revision": "3a7a7a37cc488d33060b",
+    "url": "/monster-rolodex-course/static/js/runtime-main.0865422d.js"
+  }
+]);
